1d10af4f9dee0ef7fdd8de39d42dbe3a8ecc0eb8
julian <julianmoreno242@gmail.com>
2024-03-26 21:32:56 -0500

    feat: add content

---
+++ src/utils/data.ts
@@ -0,0 +1,38 @@
+
+const services = [
+    {
+        title: "Desarrollo Web Personalizado",
+        description: "Creamos sitios web a medida que reflejen la identidad de tu marca y se adapten a tus necesidades específicas.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /></svg>`,
+    },
+    {
+        title: "Diseño de Tiendas en Línea",
+        description: "Diseñamos y desarrollamos tiendas en línea atractivas y funcionales para impulsar tus ventas en el mundo digital.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Desarrollo de Aplicaciones Web",
+        description: "Creamos aplicaciones web poderosas y escalables que mejoren la eficiencia y productividad de tu negocio.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    
+];
+
+const tecnologies = [
+    {
+        name: "Next JS",
+    },
+    {
+        name: "Angular",
+    },
+    {
+        name:"Django",
+    },
+    {
+        name:"PostgreSQL",
+    }
+]
+
+export { services,tecnologies };
